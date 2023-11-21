9f3cbc91c1c1412ffd96a92e05341b9a44e6eee8
Krystof <klenkrys@fit.cvut.cz>
2023-11-21 14:52:55 +0100

    Parser v2

--- src/main/scala/commands/LinearExecutor.scala
+++ src/main/scala/commands/LinearExecutor.scala
@@ -2,17 +2,16 @@ package commands
 
 import asciiConvertion.{AsciiTableProvider, LinearConverter}
 import core.{AsciiTable, Image}
-import filters.{Brightness, FLIP_AXIS, Flip, Invert}
+import filters.{Brightness, FLIP_AXIS, Filter, Flip, Invert}
 import loader.ImageFileLoader
 import output.{AsciiFileOutputWriter, ConsoleAsciiOutputWriter, ImageFileOutputWriter}
 import parser.CommandToken
 
 class LinearExecutor(cmdTokens: List[CommandToken]) extends Executor {
   private val order = List("image","flip","brightness","invert", "output-file","output-console")
-
+  println(cmdTokens)
   var ordered = sortCommands
   // read load
-  println("ORDERED: " + ordered)
   val option = ordered.head.option
   if( !option.equals("image") || ordered.head.args.size != 1){
     println(option)
@@ -28,44 +27,61 @@ class LinearExecutor(cmdTokens: List[CommandToken]) extends Executor {
   ordered = ordered.drop(1);
   var img = imgOpt.get
   println(ordered)
+
+  var asciiTableSelected = AsciiTableProvider.DEFAULT_TABLE
+
   for(token<-ordered){
-    token match {
-      case CommandToken("invert", _) =>
-        println("invert apply")
-        val filter = new Invert()
-        img = filter.apply(img)
+    token.option match {
 
-      case CommandToken("brightness",_) =>
-          val filter = new Brightness(80)
+      case "invert" =>
+        if(token.args.isEmpty){
+          val filter = new Invert()
           img = filter.apply(img)
+        }
 
-      case CommandToken("flip",_) =>
-        val filter = new Flip(FLIP_AXIS.X)
-        img = filter.apply(img)
+      case "brightness" =>
+        if(token.args.size==1){
+          val filter = new Brightness(token.args.head.toInt)
+          img = filter.apply(img)
+        }else{
+          throw new IllegalArgumentException
+        }
 
+      case "flip" =>
+        if(token.args.size != 1){
+          throw new IllegalArgumentException
+        }
+        if(token.args.head.equals("x")){
+          val filter = new Flip(FLIP_AXIS.X)
+          img = filter.apply(img)
+        }
+        if(token.args.head.equals("y")){
+          val filter = new Flip(FLIP_AXIS.Y)
+          img = filter.apply(img)
+        }
 
-      case CommandToken("output-console", _) =>
-        val converter = new LinearConverter(AsciiTableProvider.DEFAULT_TABLE)
+      case "table" =>
+        if(token.args.size != 1){
+          throw new IllegalArgumentException
+        }
+        asciiTableSelected = AsciiTableProvider.customTable(token.args.head.toCharArray)
+
+      case "output-console" =>
+        val converter = new LinearConverter(asciiTableSelected)
         val ascii = converter.convert(img)
         val saver = new ConsoleAsciiOutputWriter
         saver.output(ascii)
 
-      case CommandToken("output-file",_)=>
-        println("saved")
-        println(token)
-        val s = new ImageFileOutputWriter("src/test/assets/xxxxxxx.jpg","jpg")
-        s.output(img)
-        val converter = new LinearConverter(AsciiTableProvider.DEFAULT_TABLE)
+      case "output-file" =>
+        if(token.args.size != 1){
+          throw new IllegalArgumentException
+        }
+        val converter = new LinearConverter(asciiTableSelected)
         val ascii = converter.convert(img)
-        val saver = new AsciiFileOutputWriter("src/test/assets/yyy")
+        val saver = new AsciiFileOutputWriter(token.args.head)
         saver.output(ascii)
-
     }
   }
-
-
-
-
 
   private def sortCommands: List[CommandToken] = {
     cmdTokens.sortBy(token => order.indexOf(token.option))
