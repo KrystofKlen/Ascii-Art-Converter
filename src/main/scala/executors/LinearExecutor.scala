eda7b012fb43f6a45bcf11b81356f8478eb26a1a
Krystof <klenkrys@fit.cvut.cz>
2023-11-29 19:06:09 +0100

    refactoring

--- src/main/scala/executors/LinearExecutor.scala
+++ src/main/scala/executors/LinearExecutor.scala
@@ -10,65 +10,14 @@ import output.{AsciiFileOutputWriter, ConsoleAsciiOutputWriter}
 
 class LinearExecutor(cmds:List[Command[_]]) extends Executor {
   override def executeCommands(): Unit = {
-
     var imgProduct: Option[Image] = Option.empty
-    var asciiTable = AsciiTableProvider.DEFAULT_TABLE
-
-    cmds.foreach {
-      case cmd: Command[_] => {
-        cmd match {
-          case command: LoadFromFileCmd =>
-            var loader = new ImageFileLoader(command.arg.get)
-            imgProduct = loader.load()
-          case _ =>
-        }
-      }
-    }
-
-    cmds.foreach{
-      case cmd: Command[_] => {
-        cmd match {
-          case command: BrightnessCmd =>
-            imgProduct = applyFilter(new Brightness(command.arg.get),imgProduct)
-
-          case command: FlipCmd =>
-            imgProduct = applyFilter( new Flip(command.arg.get) ,imgProduct)
-
-          case command: InvertCmd =>
-            imgProduct = applyFilter( new Invert() ,imgProduct)
-
-          case command: RandomImgCmd =>
-            val imageGenerator = new RandomImageGenerator
-            imgProduct = Option(imageGenerator.generateImage())
-
-          case command: CustomTableCmd =>
-            asciiTable = AsciiTableProvider.customTable(command.arg.get.chars)
-
-          case command: PickAsciiTableCmd =>
-            asciiTable = command.arg.get
-          case _ =>
-        }
-      }
-    }
-
-    cmds.foreach{
-      case cmd: Command[_] => {
-            cmd match {
-              case command: OutputFileCmd =>
-                imgProduct = applyFilter(new Greyscale, imgProduct)
-                val outputCmd = new AsciiFileOutputWriter(command.arg.get)
-                outputCmd.output(getAsciiArt(imgProduct,asciiTable))
-              case command: OutputConsoleCmd =>
-                imgProduct = applyFilter(new Greyscale, imgProduct)
-                val outputCmd = new ConsoleAsciiOutputWriter
-                outputCmd.output(getAsciiArt(imgProduct,asciiTable))
-              case _ =>
-            }
-      }
-    }
+    imgProduct = loadData();
+    imgProduct = applyFiltes(imgProduct)
+    var asciiTable = setAsciiTable()
+    output(imgProduct,asciiTable)
   }
 
-  private def loadData(): Option[Image]  = {
+  private def loadData(): Option[Image] = {
     var imgProduct: Option[Image] = Option.empty
     cmds.foreach {
       case cmd: Command[_] => {
@@ -83,6 +32,64 @@ class LinearExecutor(cmds:List[Command[_]]) extends Executor {
     imgProduct
   }
 
+  private def applyFiltes(product: Option[Image]): Option[Image]  = {
+    var imgProduct: Option[Image] = product
+    cmds.foreach {
+      case cmd: Command[_] => {
+        cmd match {
+          case command: BrightnessCmd =>
+            imgProduct = applyFilter(new Brightness(command.arg.get), imgProduct)
+
+          case command: FlipCmd =>
+            imgProduct = applyFilter(new Flip(command.arg.get), imgProduct)
+
+          case command: InvertCmd =>
+            imgProduct = applyFilter(new Invert(), imgProduct)
+
+          case command: RandomImgCmd =>
+            val imageGenerator = new RandomImageGenerator
+            imgProduct = Option(imageGenerator.generateImage())
+
+          case _ =>
+        }
+      }
+    }
+    imgProduct
+  }
+
+  private def setAsciiTable(): AsciiTable = {
+    var asciiTable: AsciiTable = AsciiTableProvider.DEFAULT_TABLE
+        cmds.foreach {
+          case command: CustomTableCmd =>
+            asciiTable = AsciiTableProvider.customTable(command.arg.get.chars)
+
+          case command: PickAsciiTableCmd =>
+            asciiTable = command.arg.get
+
+          case _ => asciiTable = AsciiTableProvider.DEFAULT_TABLE
+        }
+    asciiTable
+  }
+
+  private def output(product: Option[Image], asciiTable: AsciiTable): Unit = {
+    var imgProduct = product
+    cmds.foreach {
+      case cmd: Command[_] => {
+        cmd match {
+          case command: OutputFileCmd =>
+            imgProduct = applyFilter(new Greyscale, imgProduct)
+            val outputCmd = new AsciiFileOutputWriter(command.arg.get)
+            outputCmd.output(getAsciiArt(imgProduct, asciiTable))
+          case command: OutputConsoleCmd =>
+            imgProduct = applyFilter(new Greyscale, imgProduct)
+            val outputCmd = new ConsoleAsciiOutputWriter
+            outputCmd.output(getAsciiArt(imgProduct, asciiTable))
+          case _ =>
+        }
+      }
+    }
+  }
+
   private def applyFilter(filter: Filter,imgProduct: Option[Image]): Option[Image] = {
     Option(filter.apply(imgProduct.getOrElse(throw new Exception)))
   }
@@ -91,4 +98,5 @@ class LinearExecutor(cmds:List[Command[_]]) extends Executor {
     val converter = new LinearConverter(asciiTable)
     converter.convert(imgProduct.getOrElse(throw new Exception()))
   }
+
 }
